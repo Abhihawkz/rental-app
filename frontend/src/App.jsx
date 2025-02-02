e96bfea2050adb08814530e1c929d6cdd947e881
Abhishek <abhipro01@gmail.com>
2025-02-02 16:39:31 +0530

    Routing sorted for Admin and User

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,26 @@ import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home.jsx';
+import AdminLayout from './layouts/AdminLayout.jsx';
+import Admin from './pages/Admin.jsx';
+import UserLayout from './layouts/UserLayout.jsx';
+import PublicLayout from './layouts/PublicLayout.jsx';
 const App = () => {
   return (
     <BrowserRouter>
     <Toaster />
       <Routes>
-        <Route path='/signin' element={<Login />} />
-        <Route path='/signup' element={<Register />}  />
-        <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<AdminLayout />}>
+        <Route index element={<Admin/>}>
+        </Route>
+        </Route>
+        <Route path='/' element={<PublicLayout/>}>
+        <Route path='signin' element={<Login />} />
+        <Route path='signup' element={<Register />}  />
+        </Route>
+        <Route path='/' element={<UserLayout />}>
+        <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
